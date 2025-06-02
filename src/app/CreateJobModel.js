7e6bbc454cdb9fb8144772f44f2be5e22d9c978f
hari605605 <your-email@example.com>
2025-06-02 17:42:44 +0530

    last

--- src/app/CreateJobModel.js
+++ src/app/CreateJobModel.js
@@ -11,7 +11,7 @@ export default function CreateJobModal({ onClose }) {
         salaryMax: "",
         deadline: "",
         description: "",
-        status: "published", // default
+        status: "published", 
     });
 
 
